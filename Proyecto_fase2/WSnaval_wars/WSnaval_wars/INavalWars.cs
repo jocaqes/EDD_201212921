5e959377ccb422a90ff3c781e47e5a96b4fa9d52
Jose Espinoza <pancho_jose_carlos@hotmail.com>
2018-01-06 10:58:50 -0600

    Fase 2 desde 0

---
+++ Proyecto_fase2/WSnaval_wars/WSnaval_wars/INavalWars.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace WSnaval_wars
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "INavalWars" in both code and config file together.
+    [ServiceContract]
+    public interface INavalWars
+    {
+
+    }
+}
---
+++ Proyecto_fase2/WSnaval_wars/WSnaval_wars/NavalWars.svc.cs
@@ -0,0 +1,9 @@
+﻿namespace WSnaval_wars
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "NavalWars" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select NavalWars.svc or NavalWars.svc.cs at the Solution Explorer and start debugging.
+    public class NavalWars : INavalWars
+    {
+
+    }
+}
